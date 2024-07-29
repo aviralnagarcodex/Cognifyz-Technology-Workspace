b95eb18a3168e17ce6a390f37800ddb9e9359cb6
Aviral Nagar <aviralnagarcodex@gmail.com>
2024-07-29 21:25:18 +0530

    Create alertbox.js

---
+++ Cognifyz Level 1 Task 2/alertbox.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good morning!";
+    } else if (hours < 18) {
+        greeting = "Good afternoon!";
+    } else {
+        greeting = "Good evening!";
+    }
+
+    alert(greeting);
+};
