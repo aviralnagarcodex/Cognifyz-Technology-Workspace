d8a24ca40b9cda1e46fb5a0065b547d105c67553
Aviral Nagar <aviralnagarcodex@gmail.com>
2024-07-29 21:25:02 +0530

    Create script.js

---
+++ Cognifyz Level 1 Task 2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("colorButton").addEventListener("click", function() {
+    // Change the button's background color
+    this.style.backgroundColor = this.style.backgroundColor === 'blue' ? 'green' : 'blue';
+});
